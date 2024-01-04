6d81ec2b79256f9a3c1335bfde04f267cc7b35d2
kjniemela <johannes@niemela.se>
2024-01-04 02:43:25 -0600

    make navbar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,12 +13,14 @@ const NavBar = () => {
   const location = useLocation();
 
   return (
-    <nav className="ma-4 d-flex align-center gap-1">
-      <Link className="link" to="/"><h1 className="ma-4 mr-8">HMI Studios</h1></Link>
-      {Object.keys(paths).map(path => (
-        <Link key={path} className={`link nav-btn ${path === location.pathname ? 'nav-selected' : ''}`} to={path}>{paths[path]}</Link>
-      ))}
-    </nav>
+    <div className="nav-box">
+      <nav className="ma-4 mb-0 d-flex align-center gap-1">
+        <Link className="link" to="/"><h1 className="ma-4 mr-8">HMI Studios</h1></Link>
+        {Object.keys(paths).map(path => (
+          <Link key={path} className={`link nav-btn ${path === location.pathname ? 'nav-selected' : ''}`} to={path}>{paths[path]}</Link>
+        ))}
+      </nav>
+    </div>
   );
 };
 
