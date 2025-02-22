87c2692e2a4f473b89c2d2e3901a2d0815aa9ff5
kjniemela <johannes@niemela.se>
2025-02-22 16:47:22 -0600

    remove budgeteer and remnant (dead projects)

--- src/views/Misc.jsx
+++ src/views/Misc.jsx
@@ -13,9 +13,9 @@ class Misc extends React.Component {
         <div className="d-flex flex-column align-center"><h1>Miscellaneous Projects</h1></div>
         <div className="d-flex flex-row flex-wrap justify-center gap-6">
           <ProjectCard small project={'archivium'} />
-          <ProjectCard small project={'remnant'} />
+          {/* <ProjectCard small project={'remnant'} /> */}
           <ProjectCard small project={'tkdmotions'} />
-          <ProjectCard small project={'budgeteer'} />
+          {/* <ProjectCard small project={'budgeteer'} /> */}
           <ProjectCard small project={'simplesequencer'} />
           <ProjectCard small project={'voteinator'} />
         </div>
